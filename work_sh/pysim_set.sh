442bb0080eec001a538a4f65474dd9d00a871876
sooof <sooof@me.com>
2017-05-29 16:23:52 -0700

    add work_sh/pysim_set.sh

---
+++ work_sh/pysim_set.sh
@@ -0,0 +1,6 @@
+
+brew install wget
+wget https://github.com/sooof/me_doc/tree/master/openbts/pysim_readme.md
+git clone git://git.osmocom.org/pysim.git
+cd pysim
+git reset --hard 6e58914746af82314361fc9d9eae6cecbbe2e64b
